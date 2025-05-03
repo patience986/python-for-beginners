ba25b50497b3b3a1fdd19614908f55b06b504afb
pa <nalweisopatience@gmail.com>
2025-05-03 16:13:25 -0700

    patie.py

---
+++ patie.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("concatenate"+"lion")
+print("hello","there")
+print('he said"Hi"')
+print(1+3)
+print(1-3)
+print(1/3)
+print(1*3)
+# /*variables*/  <-- This is a comment in Python, not the start of a multi-line comment like in some other languages.
+exVar = 100
+print(exVar)
+       # ...the 'print' line *must* be indented relative to the 'for' line.
+     
